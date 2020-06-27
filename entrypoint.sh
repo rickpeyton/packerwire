fa897aa9e36e353d45d22f22b21066efff5a7a2e
Rick Peyton <peytorb@gmail.com>
2020-06-27 18:10:26 -0500

    Add context to lambda handler, fix entrypoint (#3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-bundle config set deployment 'true'
+bundle config deployment true
+bundle config without development
 bundle install --jobs 8
 zip -r packerwire.zip lambda_function.rb app vendor
