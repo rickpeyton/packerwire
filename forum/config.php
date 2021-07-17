4ec8bb58c5eacbc60f2b819090c63f210f41d9b5
Rick Peyton <peytorb@gmail.com>
2021-07-17 19:39:02 +0000

    Add back the forum files

---
+++ forum/config.php
@@ -0,0 +1,16 @@
+<?php
+// phpBB 3.3.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\postgres';
+$dbhost = $_ENV["DATABASE_HOST"];
+$dbport = '';
+$dbname = $_ENV["DATABASE_NAME"];
+$dbuser = $_ENV["DATABASE_USER"];
+$dbpasswd = $_ENV["DATABASE_PASSWORD"];
+$table_prefix = 'bb_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
